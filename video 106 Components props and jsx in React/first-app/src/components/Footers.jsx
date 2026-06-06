6e936b658dcc6cd3f3f60a98bf3d24f1a8ddcc91
RAJMAHMMAD0786 <rajalam05588@gmail.com>
2026-06-06 16:14:10 +0530

    Today i revise the Components and props concepts

--- video 106 Components props and jsx in React/first-app/src/components/Footers.jsx
+++ video 106 Components props and jsx in React/first-app/src/components/Footers.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+
 import "./Footers.css"
 
 const Footers = () => {
   return (
     <div className='Footers'>
-      Raj Mahmmad www.google.com||All right and fine
+      Copyright &copy; www.google.com||All right and fine
     </div>
   )
 }
